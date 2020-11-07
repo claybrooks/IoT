b2d3c72319629cbc8c0d146e406d9799e976f49d
Adam Sittler <SittlerAM@gmail.com>
2020-11-06 19:59:52 -0500

    turned into class

--- aws/data_link.py
+++ aws/data_link.py
@@ -1,43 +1,44 @@
 import boto3
 from pprint import pprint
 
-def get_spot(table, Location, Space, dynamodb=None):
+class aws_link():
+    def __init__(self):
+        self.dynamodb = boto3.resource('dynamodb')
+        self.table = self.dynamodb.Table('Smart_Park')
 
-    try:
-        response = table.get_item(Key={'Location': Location, 'Spot': Space})
-        print("Got Correct Spot")
-    except Exception as e:
-        print(e)
-    else:
-        return response['Item']
+    def get_spot(self, Location, Space):
+        try:
+            response = self.table.get_item(Key={'Location': Location, 'Spot': Space})
+            print("Got Correct Spot")
+        except Exception as e:
+            print(e)
+        else:
+            return response['Item']
 
-def put_spot(table, Location, Space, Occupied, dynamodb=None):   
-
-    response = table.put_item(
-       Item={
-            'Location': Location,
-            'Spot': Space,
-            'Occupied': Occupied
-        }
-    )
-
-    return response
+    def put_spot(self, Location, Space, Occupied):   
+        try:
+            response = self.table.put_item(
+            Item={
+                    'Location': Location,
+                    'Spot': Space,
+                    'Occupied': Occupied
+                }
+            )
+        except Exception as e:
+            print(e)
+        else:
+            return response
 
 if __name__ == '__main__':
 
-    dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('Smart_Park')
+    aws = aws_link()
 
-    request_info = get_spot(table, "Home", 1,)
-    if request_info:
+    response_info = aws.get_spot("Home", 1,)
+    if response_info:
         print("Get Spot succeeded:")
-        pprint(request_info)
+        pprint(response_info)
 
-    request_info = put_spot(table, "Home", 2, 'false')
-    if request_info:
+    response_info = aws.put_spot("Home", 3, 'true')
+    if response_info:
         print("Put Spot succeeded:")
-        pprint(request_info)
-
-    
-
-    
+        pprint(response_info)

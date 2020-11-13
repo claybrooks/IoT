feabb89848d6d5dd6fc8409829b803e56c9f1dea
Clay Brooks <clay_brooks@outlook.com>
2020-11-13 11:51:31 -0500

    updated ui to refresh on data change

--- app/iot/iot/iot/Models/Item.cs
+++ app/iot/iot/iot/Models/Item.cs
@@ -1,11 +1,41 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace iot.Models
 {
-    public class Item
+    public class Item : INotifyPropertyChanged
     {
-        public string Location { get; set; }
-        public string Spot { get; set; }
-        public bool Occupied { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+
+        }
+
+        private string _Location = "";
+        public string Location 
+        { 
+            get { return _Location; }
+            set { _Location = value; OnPropertyChanged("Location"); }
+        }
+
+        private string _Spot = "";
+        public string Spot
+        {
+            get { return _Spot; }
+            set { _Spot = value; OnPropertyChanged("Spot"); }
+        }
+
+        private bool _Occupied = false;
+        public bool Occupied
+        {
+            get { return _Occupied; }
+            set { _Occupied = value; OnPropertyChanged("Occupied"); }
+        }
     }
 }
